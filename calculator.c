62d72497208c49c9be68a2e2f7fae238cecbfa57
engnourmo <nourgharablyzero@gmail.com>
2025-05-04 16:55:29 +0300

    modified Devide function and reminder function

--- calculator.c
+++ calculator.c
@@ -22,56 +22,67 @@ int factorial_f(int num);
 int main(){
     printf("+ -> add\n- -> sub\n* -> mul\n/ -> devide\n& -> logic AND\n| -> logic OR\n~ -> loguc NOT\n^ -> logic XOR\nR -> remider\nI -> increment\nD -> decrement\nF ->factorial\n");
     printf("enter the operand : ");
-    scanf("%c", &operator); 
-    if(operator == 'I' || operator == 'D' || operator == 'F' || operator == '~'){
-        printf("enter your first number : ");
-        scanf("%i",&number_1);
+    scanf("%c", &operator);
+    if((operator == 'F') || (operator == 'I') || (operator == '~') || (operator == 'D')){
+        printf("enter you number : ");
+        scanf("%i", &number_1);
     }
     else{
         printf("enter your first number : ");
-        scanf("%i",&number_1);
+        scanf("%i", &number_1);
         printf("enter your second number : ");
         scanf("%i", &number_2);
     }
     switch(operator){
-        case ('+') :
-            printf("Result: %d\n", add_f(number_1, number_2)); 
+        case '+' :
+            printf("Result = %i\n", add_f(number_1, number_2));
             break;
-        case ('-') :
-            printf("Result: %d\n", sub_f(number_1, number_2));
+        case '-' :
+            printf("Result = %i\n", sub_f(number_1, number_2));
             break;
-        case ('*') :
-            printf("Result: %d\n", mul_f(number_1, number_2)); 
+        case '*' :
+            printf("Result = %i\n", mul_f(number_1, number_2));
             break;
-        case ('/') :
-            printf("Result: %d\n", devide_f(number_1, number_2)); 
+        case '/' :
+            if(0 == number_2){
+                printf("Invalid deide by 0\n");
+            }
+            else{
+                printf("Result = %i\n", devide_f(number_1, number_2));
+            }
             break;
-        case ('&') :
-            printf("Result: %d\n", l_AND_f(number_1, number_2)); 
+        case '&' :
+            printf("Result = %i\n", l_AND_f(number_1, number_2));
             break;
-        case ('|') :
-            printf("Result: %d\n", L_OR_f(number_1, number_2)); 
+        case '|':
+            printf("Result = %i\n", L_OR_f(number_1, number_2));
             break;
-        case ('~') :
-            printf("Result: %d\n", l_not_f(number_1)); 
+        case '~':
+            printf("Result = %i\n",l_not_f(number_1));
             break;
-        case ('^') :
-            printf("Result: %d\n", XOR_f(number_1, number_2)); 
+        case '^' :
+            printf("Result = %i\n", XOR_f(number_1, number_2));
             break;
-        case ('R') :
-            printf("Result: %d\n", Reminder_f(number_1, number_2)); 
+        case 'R' :
+            if(0 == number_2){
+                printf("Invalid devide by 0\n");
+            }
+            else{
+                printf("Result = %i\n", Reminder_f(number_1, number_2));
+            }
             break;
-        case ('I') :
-            printf("Result: %d\n", increment_f(number_1)); 
+        case 'I' :
+            printf("Reuslt = %i\n", increment_f(number_1));
             break;
-        case ('D') :
-            printf("Result: %d\n", decrement_f(number_1)); 
+        case 'D' :
+            printf("Result = %i\n", decrement_f(number_1));
             break;
-        case ('F') :
-            printf("Result: %d\n", factorial_f(number_1));
+        case 'F' :
+            printf("Result = %i\n", factorial_f(number_1));
             break;
-        default :
-            printf("invalid operator!!\n");
+        default:
+            printf("Ivalid Operator!!");
+            break;
     }
     return 0;
 }
@@ -90,14 +101,7 @@ int mul_f(int num1,int num2){
 }
 
 int devide_f(int num1,int num2){
-    int tempnum = 0;
-    if(num2 == 0){
-        printf("invlaid number2\n");
-    }
-    else{
-        tempnum = (num1 / num2);
-    }
-    return tempnum;
+        return (num1 / num2);
 }
 
 int l_AND_f(int num1,int num2){
@@ -117,14 +121,7 @@ int XOR_f(int num1,int num2){
 }
 
 int Reminder_f(int num1, int num2){
-    int tempnum = 0;
-    if(num2 == 0){
-        printf("invlaid number2\n");
-    }
-    else{
-        tempnum = (num1 % num2);
-    }
-    return tempnum;
+    return (num1 % num2);
 }
 
 int increment_f(int num){
